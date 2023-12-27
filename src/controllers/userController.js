0d9f851a7599eda0e18a17feb6a72ce20fb0a5d2
kietpt2003 <tuankiet29012003@gmail.com>
2023-12-27 16:05:29 +0700

    fix some bug on get all user

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -23,10 +23,16 @@ let handleLogin = async (req, res) => {
 
 let handleAllUsers = async (req, res) => {
     let id = req.body.id; //ALL or id
+
+    if (!id) {
+        return res.status(200).json({
+            errCode: 1,
+            errMessage: 'Missing required parameters',
+            users: []
+        })
+    }
+
     let users = await userService.getAllUsers(id);
-
-    console.log(users);
-
     return res.status(200).json({
         errCode: 0,
         errMessage: 'OK',
