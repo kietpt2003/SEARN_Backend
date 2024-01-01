fea320e98d0cb5d904037ab9d8721f7e3d07b8a8
kietpt2003 <tuankiet29012003@gmail.com>
2024-01-01 20:33:23 +0700

    create getAllCode function

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -65,10 +65,24 @@ let handleDeleteUser = async (req, res) => {
     return res.status(200).json(message);
 }
 
+let getAllCode = async (req, res) => {
+    try {
+        let data = await userService.getAllCodeService(req.query.type);
+        return res.status(200).json(data)
+    } catch (error) {
+        console.log('Error: ', error)
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: 'Error from server'
+        })
+    }
+}
+
 module.exports = {
     handleLogin,
     handleAllUsers,
     handleCreateNewUser,
     handleEditUser,
-    handleDeleteUser
+    handleDeleteUser,
+    getAllCode
 }
